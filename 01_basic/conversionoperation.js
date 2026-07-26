37937839d305876f8a49f370e26086f7d4df7584
Kunal <knlkunal242@gmail.com>
2026-07-26 11:16:21 +0530

    now we reach in type conversion, it most inportant in any programing language.

---
+++ 01_basic/conversionoperation.js
@@ -0,0 +1,16 @@
+let score = true
+
+console.log(typeof(score));
+console.log(typeof(score));
+
+let valueinnumber = Number(score)
+console.log(valueinnumber);
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1
+// 
+
+let isloggedin = 1
+let booleanvalue = Boolean(isloggedin)
+console.log(booleanvalue);
