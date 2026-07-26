2dc1598e1891e7c8fdabc7c8ab2d6e933a43fdc0
Kunal <knlkunal242@gmail.com>
2026-07-26 11:14:59 +0530

    now we reach in datatype

---
+++ 01_basic/datatype.js
@@ -0,0 +1,15 @@
+let name = "Radha"
+let age = 19
+let isLoggedIn = true
+
+// Number = 2 TO power of 52
+//bigint = 1234567890123456789012345678901234567890n
+// String = "Hello World"
+// Boolean = true or false
+// Null = null(standalone value)
+// Undefined = undefined(standalone value)
+// Symbol = Symbol("id")(unique and immutable value)
+
+//object = {key: value} // key-value pair
+console.log(typeof undefined)
+console.log(typeof null) // object
