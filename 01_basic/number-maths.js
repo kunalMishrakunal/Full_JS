1e08c939fabfa6ed5c574dbc98951bcd78eed694
Kunal <knlkunal242@gmail.com>
2026-07-28 11:13:16 +0530

    numbers in js

---
+++ 01_basic/number-maths.js
@@ -0,0 +1,26 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const othernumber = 23.8966
+
+console.log(othernumber.toPrecision(3));
+
+const hundreds = 1000000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+
+// -------------Maths---------------
+
+console.log(Math);
+console.log(Math.abs(-4))
+console.log(Math.round(4.6))
+console.log(Math.ceil(4.2))
+
+//-----------------------------------
+
+console.log(Math.random());
+console.log(Math.random()*10+11);
