b6c1bb347dd2819611dc0b9e5ba4da0f78304246
Kunal <knlkunal242@gmail.com>
2026-07-28 11:14:55 +0530

    string in js

---
+++ 01_basic/string.js
@@ -0,0 +1,17 @@
+const name = "hitesh"
+const repocount = 50
+
+console.log(name + repocount + "Value");
+
+console.log(`Hello my name is ${name} and my repo is ${repocount}`);
+
+
+const gamename = new String("Radharani")
+
+console.log(gamename);
+console.log(gamename[0]);
+
+console.log(gamename.__proto__);
+
+const newstring = gamename.slice(-8, 4)
+console.log(newstring);
