46c8936d4ec5d6aacb802c106da6729b3985c0a2
Kunal <knlkunal242@gmail.com>
2026-07-28 11:13:44 +0530

    operations in js

---
+++ 01_basic/operation.js
@@ -0,0 +1,16 @@
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "World"
+
+let str3 = str1 + " " + str2
+console.log(str3);
