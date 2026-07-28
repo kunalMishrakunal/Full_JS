e73fa71aadcc002868ff45cfbfbab2d63bc3cac9
Kunal <knlkunal242@gmail.com>
2026-07-28 11:12:13 +0530

    data types in js like premitive and reference

---
+++ 01_basic/datatype_2.js
@@ -0,0 +1,26 @@
+// Primitive =>
+
+// 7 types : String, number, boolean, null, undefined, symbol, bigint
+
+
+
+//Reference (Non primitive) =>
+
+// Array, Object, Functions
+
+const heros = ['Ran', 'Lakshman', 'Hanuman', 'Krishna']
+
+let shreeji = {
+    name: "Radha",
+    Age: 19,
+
+}
+
+const myfunction = function(){
+    console.log("Radha, Radha");
+}
+myfunction();
+
+console.log(shreeji);
+
+
